3d3b016171c102e932c1d2729159bd5ded1f995f
Togrul97 togrul-Dev <sultanzadetogrul@gmail.com>
2023-10-21 21:27:26 +0000

    react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function App() {
+
+const [a,seta] = useState(0)
+
+  return (
+    <div className="app">
+      <h1>{a}</h1>
+      <button onClick={() => seta((t) =>t+1)}>+</button>
+      <button onClick={() => seta((t) =>t-1)}>-</button>
+    </div>
+  );
+}
+
+export default App;
+
